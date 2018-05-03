5838326162c63bfcf99b5ad13c4384d95d04c245
Mytho145 <38844887+Mytho145@users.noreply.github.com>
2018-05-02 21:40:03 -0300

    Update noticias.php

--- noticias.php
+++ noticias.php
@@ -1,55 +1,8 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Notícias</title>
-    <style>
-      .data{
-        color : red;
-        font-size: 20px;
-        font-weight: bold;
-        font-family: inherit;
-      }
-      .peq{
-          font-size: 13px;
-          font-family: inherit;
-        }
-      .bbold{
-        font-weight: bold;
-      }
-      .clearm{
-        padding-top: 5px;
-        padding-bottom: 5px;
-      }
-    </style>
-  </head>
-  <body>
-
-
-      <li class="data">02/05/2018</li>
-      <li class="peq">GodWare 1° atualização.</li>
-      <div class="clearm"></div>
-
-      <li class="peq"><span class="bbold">[+]Legit AntAim</span></li>
-
-      <div class="clearm"></div>
-
-
-      <li class="peq"><span class="bbold">[+]Inventory Unlocker</span></li>
-      <div class="clearm"></div>
-
-
-      <li class="peq"><span class="bbold">[+]Radar "Only InGame"</span></li>
-      <div class="clearm"></div>
-
-      <li class="peq"><span class="bbold">[-]Fake Lag</span></li>
-      <div class="clearm"></div>
-
-      <li class="peq"><span class="bbold">[-]Slide Walk</span></li>
-      <div class="clearm"></div>
-
-      <li class="peq"><span class="bbold">[!]KnifeBot (fixed)</span></li>
-      <div class="clearm"></div>
-
-  </body>
-</html>
+02/05/2018
+GodWare 1° atualização.
+[+]Legit AntAim
+[+]Inventory Unlocker
+[+]Radar "Only InGame"
+[-]Fake Lag
+[-]Slide Walk
+[!]KnifeBot (fixed)
